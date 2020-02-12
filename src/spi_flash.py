f947cc361e1c17c07939216c6e7ffe85147226f6
justapig9020 <justapig9020@gmail.com>
2020-02-12 17:04:49 +0800

    modify coding feature and add try/catch

--- src/spi_flash.py
+++ src/spi_flash.py
@@ -1,6 +1,6 @@
-import spidev 
-from cmd import *
-from time import *
+import spidev
+import cmd
+import time
 
 def is_set_flag(reg, flag):
     return reg & flag == flag
@@ -8,6 +8,8 @@ def is_set_flag(reg, flag):
 class spi_flash:
     mem = spidev.SpiDev()
     delay = 0.5
+    bus = None
+    dev_num = None
 
     def __init__(self, bus, dev_num, speed = 1000000, mode = 0):
         self.bus = bus
@@ -21,19 +23,19 @@ class spi_flash:
 
     def read(self, addr, size):
         for i in range(10):
-            self.mem.xfer([WDI])
-            sr1 = self.get_reg(RSR1) 
-            if is_set_flag(sr1, SR1_WEL | SR1_BUSY) == False:
+            self.mem.xfer([cmd.WDI])
+            sr1 = self.get_reg(cmd.RSR1) 
+            if is_set_flag(sr1, cmd.SR1_WEL | cmd.SR1_BUSY) == False:
                 break
-            sleep(self.delay)
+            time.sleep(self.delay)
 
-        if is_set_flag(sr1, SR1_WEL | SR1_BUSY):
+        if is_set_flag(sr1, cmd.SR1_WEL | cmd.SR1_BUSY):
             return [0x00]
         else:
-            send_buf = [RD]
+            send_buf = [cmd.RD]
             send_buf += addr
             for i in range(size):
-                send_buf += [NOP]
+                send_buf += [cmd.NOP]
 
             recv_buf = self.mem.xfer(send_buf)
             for i in range(4):
@@ -41,7 +43,7 @@ class spi_flash:
         return recv_buf
 
     def get_jid(self):
-        return self.send_cmd(self.mem, RJID, 3)
+        return self.send_cmd(self.mem, cmd.RJID, 3)
 
     def chk_dev(self):
         ret = self.get_jid()
@@ -50,7 +52,7 @@ class spi_flash:
         return mid == 0xEF and jid == [0x40, 0x16]
 
     def set_reg(self, cmd, val):
-        self.mem.xfer(WENVSR)
+        self.mem.xfer(cmd.WENVSR)
         send_buf = [cmd]
         send_buf += [val]
         self.mem.xfer(send_buf)
@@ -62,7 +64,7 @@ class spi_flash:
         send_buf = []
         send_buf += [cmd]
         for i in range(ret_size):
-            send_buf += [NOP]
+            send_buf += [cmd.NOP]
         ret = self.mem.xfer(send_buf)
         ret.pop(0)
         return ret
@@ -70,17 +72,17 @@ class spi_flash:
 
     def set_write_enable(self):
         while True:
-            sr1 = self.get_reg(RSR1)
-            if is_set_flag(sr1, SR1_BUSY) == False:
+            sr1 = self.get_reg(cmd.RSR1)
+            if is_set_flag(sr1, cmd.SR1_BUSY) == False:
                 break;
-            sleep(self.delay)
+            time.sleep(self.delay)
 
         for i in range(10):
-            self.mem.xfer([WEN])
-            sr1 = self.get_reg(RSR1) 
-            if is_set_flag(sr1, SR1_WEL):
+            self.mem.xfer([cmd.WEN])
+            sr1 = self.get_reg(cmd.RSR1) 
+            if is_set_flag(sr1, cmd.SR1_WEL):
                 return True
-            sleep(self.delay)
+            time.sleep(self.delay)
         return False
 
 
@@ -88,7 +90,7 @@ class spi_flash:
         if self.set_write_enable() == False:
             return False
     
-        send_buf = [PP]
+        send_buf = [cmd.PP]
         send_buf += addr
         send_buf += data
         self.mem.xfer(send_buf)
@@ -98,7 +100,7 @@ class spi_flash:
         if self.set_write_enable() == False:
             return False
 
-        send_buf = [SE]
+        send_buf = [cmd.SE]
         send_buf += addr
         self.mem.xfer(send_buf)
         return True
--- src/test.py
+++ src/test.py
@@ -1,4 +1,5 @@
 from spi_flash import spi_flash
+import sys
 
 def test_func(mem, addr, data):
     if mem.chk_dev():
@@ -32,17 +33,28 @@ def test_func(mem, addr, data):
     return True
 
 def test():
-    addr = [0x1f, 0x00, 0x00]
-    data = [0x09, 0x05, 0x02, 0x07]
-    mem = spi_flash(0, 0)
-    mem.setup(speed=1100000, mode=3)
+    try:
+        addr = [0x1f, 0x00, 0x00]
+        data = [0x09, 0x05, 0x02, 0x07]
+        mem = spi_flash(0, 0)
+        mem.setup(speed=1100000, mode=3)
 
-    mem.reset()
+        mem.reset()
 
-    if test_func(mem, addr, data):
-        print ("R/W test sucessed")
-    else:
-        print ("R/W test failed")
+        if test_func(mem, addr, data):
+            print ("R/W test sucessed")
+        else:
+            print ("R/W test failed")
+    except Exception as err:
+        err_cls = err.__class__.__name__
+        detail = err.args[0]
+        cl, exc, tb = sys.exc_info()
+        last_call_stk = traceback.extract_tb(tb)[-1]
+
+        file_name = lastCallStack[0]
+        line_num = lastCallStack[1]
+        func_name = lastCallStack[2]
+        print ("File: ", file_name, " line: ", line_num, " in function: ", func_name, ", ", err_cls, detail)
 
 if __name__ == "__main__":
     test()
