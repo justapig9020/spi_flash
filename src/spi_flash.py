6fb89abc54ff6651be65bbbc4d70e4685ecb38b8
justapig9020 <justapig9020@gmail.com>
2020-02-12 17:18:59 +0800

    fix cmd name space bug
    now test script is working

--- src/spi_flash.py
+++ src/spi_flash.py
@@ -1,5 +1,5 @@
 import spidev
-import cmd
+import cmd as spi_cmd
 import time
 
 def is_set_flag(reg, flag):
@@ -23,19 +23,19 @@ class spi_flash:
 
     def read(self, addr, size):
         for i in range(10):
-            self.mem.xfer([cmd.WDI])
-            sr1 = self.get_reg(cmd.RSR1) 
-            if is_set_flag(sr1, cmd.SR1_WEL | cmd.SR1_BUSY) == False:
+            self.mem.xfer([spi_cmd.WDI])
+            sr1 = self.get_reg(spi_cmd.RSR1) 
+            if is_set_flag(sr1, spi_cmd.SR1_WEL | spi_cmd.SR1_BUSY) == False:
                 break
             time.sleep(self.delay)
 
-        if is_set_flag(sr1, cmd.SR1_WEL | cmd.SR1_BUSY):
+        if is_set_flag(sr1, spi_cmd.SR1_WEL | spi_cmd.SR1_BUSY):
             return [0x00]
         else:
-            send_buf = [cmd.RD]
+            send_buf = [spi_cmd.RD]
             send_buf += addr
             for i in range(size):
-                send_buf += [cmd.NOP]
+                send_buf += [spi_cmd.NOP]
 
             recv_buf = self.mem.xfer(send_buf)
             for i in range(4):
@@ -43,7 +43,7 @@ class spi_flash:
         return recv_buf
 
     def get_jid(self):
-        return self.send_cmd(self.mem, cmd.RJID, 3)
+        return self.send_cmd(self.mem, spi_cmd.RJID, 3)
 
     def chk_dev(self):
         ret = self.get_jid()
@@ -52,7 +52,7 @@ class spi_flash:
         return mid == 0xEF and jid == [0x40, 0x16]
 
     def set_reg(self, cmd, val):
-        self.mem.xfer(cmd.WENVSR)
+        self.mem.xfer(spi_cmd.WENVSR)
         send_buf = [cmd]
         send_buf += [val]
         self.mem.xfer(send_buf)
@@ -64,7 +64,7 @@ class spi_flash:
         send_buf = []
         send_buf += [cmd]
         for i in range(ret_size):
-            send_buf += [cmd.NOP]
+            send_buf += [spi_cmd.NOP]
         ret = self.mem.xfer(send_buf)
         ret.pop(0)
         return ret
@@ -72,15 +72,15 @@ class spi_flash:
 
     def set_write_enable(self):
         while True:
-            sr1 = self.get_reg(cmd.RSR1)
-            if is_set_flag(sr1, cmd.SR1_BUSY) == False:
+            sr1 = self.get_reg(spi_cmd.RSR1)
+            if is_set_flag(sr1, spi_cmd.SR1_BUSY) == False:
                 break;
             time.sleep(self.delay)
 
         for i in range(10):
-            self.mem.xfer([cmd.WEN])
-            sr1 = self.get_reg(cmd.RSR1) 
-            if is_set_flag(sr1, cmd.SR1_WEL):
+            self.mem.xfer([spi_cmd.WEN])
+            sr1 = self.get_reg(spi_cmd.RSR1) 
+            if is_set_flag(sr1, spi_cmd.SR1_WEL):
                 return True
             time.sleep(self.delay)
         return False
@@ -90,7 +90,7 @@ class spi_flash:
         if self.set_write_enable() == False:
             return False
     
-        send_buf = [cmd.PP]
+        send_buf = [spi_cmd.PP]
         send_buf += addr
         send_buf += data
         self.mem.xfer(send_buf)
@@ -100,7 +100,7 @@ class spi_flash:
         if self.set_write_enable() == False:
             return False
 
-        send_buf = [cmd.SE]
+        send_buf = [spi_cmd.SE]
         send_buf += addr
         self.mem.xfer(send_buf)
         return True
--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 from spi_flash import spi_flash
 import sys
+import traceback
 
 def test_func(mem, addr, data):
     if mem.chk_dev():
@@ -36,6 +37,7 @@ def test():
     try:
         addr = [0x1f, 0x00, 0x00]
         data = [0x09, 0x05, 0x02, 0x07]
+
         mem = spi_flash(0, 0)
         mem.setup(speed=1100000, mode=3)
 
@@ -51,9 +53,9 @@ def test():
         cl, exc, tb = sys.exc_info()
         last_call_stk = traceback.extract_tb(tb)[-1]
 
-        file_name = lastCallStack[0]
-        line_num = lastCallStack[1]
-        func_name = lastCallStack[2]
+        file_name = last_call_stk[0]
+        line_num = last_call_stk[1]
+        func_name = last_call_stk[2]
         print ("File: ", file_name, " line: ", line_num, " in function: ", func_name, ", ", err_cls, detail)
 
 if __name__ == "__main__":
